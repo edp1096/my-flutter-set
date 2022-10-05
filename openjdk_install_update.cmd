2a7968d4985df9d87f71092feabf9c6ac6a512f2
edp1096 <edp1096@naver.com>
2022-10-06 01:47:34 +0900

    Edit openjdk.cmd

--- openjdk_install_update.cmd
+++ openjdk_install_update.cmd
@@ -1,8 +1,8 @@
 @rem JAVA 11 - not work with AVD Manager
-@rem curl -Lo openjdk.zip https://download.java.net/java/GA/jdk11/9/GPL/openjdk-11.0.2_windows-x64_bin.zip
+curl -Lo openjdk.zip https://download.java.net/java/GA/jdk11/9/GPL/openjdk-11.0.2_windows-x64_bin.zip
 
 @rem JAVA 1.8
-curl -Lo openjdk.zip https://github.com/ojdkbuild/ojdkbuild/releases/download/java-1.8.0-openjdk-1.8.0.332-1.b09/java-1.8.0-openjdk-1.8.0.332-1.b09.ojdkbuild.windows.x86_64.zip
+@rem curl -Lo openjdk.zip https://github.com/ojdkbuild/ojdkbuild/releases/download/java-1.8.0-openjdk-1.8.0.332-1.b09/java-1.8.0-openjdk-1.8.0.332-1.b09.ojdkbuild.windows.x86_64.zip
 
 tar -xf openjdk.zip
 
@@ -10,6 +10,6 @@ tar -xf openjdk.zip
 ren jdk-11.0.2 openjdk
 
 @rem JAVA 1.8
-ren java-1.8.0-openjdk-1.8.0.332-1.b09.ojdkbuild.windows.x86_64 openjdk
+@rem ren java-1.8.0-openjdk-1.8.0.332-1.b09.ojdkbuild.windows.x86_64 openjdk
 
 del openjdk.zip /q /s 1>nul
