dbc8b33039b52f6162147b757b9adc6ea7655688
edp1096 <edp1096@naver.com>
2023-08-27 17:54:03 +0900

    Add go - delete

--- delete_all.cmd
+++ delete_all.cmd
@@ -8,11 +8,13 @@ rmdir cmdline-tools /q/s 2>nul
 rmdir flutter /q/s 2>nul
 rmdir vscode /q/s 2>nul
 rmdir git /q/s 2>nul
+rmdir go /q/s 2>nul
 
 del vscode.zip /q/s 2>nul
 del git.zip /q/s 2>nul
 del flutter.zip /q/s 2>nul
 del openjdk.zip /q/s 2>nul
+del go.zip /q/s 2>nul
 
 del "Flutter Code.lnk" 2>nul
 del "%UserProfile%\desktop\Flutter Code.lnk" 2>nul
